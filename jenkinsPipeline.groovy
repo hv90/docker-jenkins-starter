6970127f9689fb4170f20b57f9c17394d52133b9
hv90 <hugomachado182@gmail.com>
2025-01-16 20:49:52 +0000

    update Jenkinsfile, Makefile, groovy file

--- jenkinsPipeline.groovy
+++ jenkinsPipeline.groovy
@@ -14,7 +14,7 @@ def jobName = "my-job"
 // } else {
 //     println "Invalid or missing directory."
 // }
-
+def hello = new File('/var/jenkins_home/workspace/project/Jenkinsfile').text
 def pipelineDefinition = new File('/var/jenkins_home/workspace/project/Jenkinsfile').text
 
 def jenkins = Jenkins.instance
